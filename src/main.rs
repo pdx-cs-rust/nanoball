6f25d152460bb575f9e70e148a0cf3edb6991686
Bart Massey <bart.massey@gmail.com>
2020-03-11 01:11:40 -0700

    finished working nanoball demo

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #![no_std]
 #![no_main]
 
+const FRAME_TIME: u32 = 20;
+
 use panic_halt as _;
 
 use riscv_rt::entry;
 use gd32vf103xx_hal::pac;
 use gd32vf103xx_hal::prelude::*;
+use gd32vf103xx_hal::delay::McycleDelay;
 use longan_nano::lcd_pins;
 use longan_nano::lcd::Lcd;
+use embedded_hal::blocking::delay::DelayMs;
 use embedded_graphics::prelude::*;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::primitives::Rectangle;
@@ -38,6 +42,8 @@ fn main() -> ! {
     let (width, height) = (160, 80);
     let (ball_width, ball_height) = (4, 4);
 
+    let mut delay = McycleDelay::new(&rcu.clocks);
+
     // Clear screen
     draw_rect(
         &mut lcd,
@@ -46,14 +52,38 @@ fn main() -> ! {
         0u16,
     );
 
-    // Draw white square at center.
-    let (left, top) = ((width - ball_width) / 2, (height - ball_height) / 2);
-    draw_rect(
-        &mut lcd,
-        (left, top),
-        (left + ball_width - 1, top + ball_height - 1),
-        0xffffu16,
-    );
+    let mut left = 1;
+    let mut top = 1;
+    let mut dx = 1;
+    let mut dy = 1;
+    loop {
+        // Erase ball.
+        draw_rect(
+            &mut lcd,
+            (left, top),
+            (left + ball_width - 1, top + ball_height - 1),
+            0u16,
+        );
 
-    loop {}
+        // Update ball position.
+        if left <= 0 || left + ball_width >= width {
+            dx = -dx;
+        }
+        left += dx;
+        if top <= 0 || top + ball_height >= height {
+            dy = -dy;
+        }
+        top += dy;
+
+        // Draw ball.
+        draw_rect(
+            &mut lcd,
+            (left, top),
+            (left + ball_width - 1, top + ball_height - 1),
+            0xffffu16,
+        );
+
+        // Delay to show ball.
+        delay.delay_ms(FRAME_TIME);
+    }
 }
