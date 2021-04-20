27e179299de96b1d77cc11dcec6c0b1a58a23ed9
Bart Massey <bart.massey@gmail.com>
2021-04-20 00:06:13 -0700

    brought dependencies up to date

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,37 @@ const FRAME_TIME: u32 = 20;
 use panic_halt as _;
 
 use riscv_rt::entry;
-use gd32vf103xx_hal::pac;
-use gd32vf103xx_hal::prelude::*;
-use gd32vf103xx_hal::delay::McycleDelay;
+use longan_nano::hal::{pac, prelude::*, delay::McycleDelay};
 use longan_nano::lcd_pins;
-use longan_nano::lcd::Lcd;
+use longan_nano::lcd::{self, Lcd};
 use longan_nano::led::{Led, rgb};
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_graphics::prelude::*;
-use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::pixelcolor::{Rgb565, raw::RawU16};
 use embedded_graphics::primitives::Rectangle;
+use embedded_graphics::primitive_style;
 
 fn draw_rect<C>(lcd: &mut Lcd, ul: (i32, i32), lr: (i32, i32), c: C)
     where C: Into<Rgb565>
 {
-    lcd
-        .draw(Rectangle::new(
-            Coord::new(ul.0, ul.1),
-            Coord::new(lr.0, lr.1),
-        )
-        .fill(Some(c.into())));
+    let rect =  Rectangle::new(
+        Point::new(ul.0, ul.1),
+        Point::new(lr.0, lr.1),
+    );
+    let _ = rect
+        .into_styled(primitive_style!(fill_color = c.into()))
+        .draw(lcd);
 }
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
 
-    // Configure clocks
+    // Configure clocks.
     let mut rcu = dp.RCU.configure().ext_hf_clock(8.mhz()).sysclk(108.mhz()).freeze();
 
+    // Take peripherals.
+    let mut afio = dp.AFIO.constrain(&mut rcu);
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpiob = dp.GPIOB.split(&mut rcu);
     let gpioc = dp.GPIOC.split(&mut rcu);
@@ -43,7 +45,7 @@ fn main() -> ! {
     let mut leds: [&mut dyn Led; 3] = [&mut red, &mut green, &mut blue];
 
     let lcd_pins = lcd_pins!(gpioa, gpiob);
-    let mut lcd = Lcd::new(dp.SPI0, lcd_pins, &mut rcu);
+    let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
     let (width, height) = (160, 80);
     let (ball_width, ball_height) = (4, 4);
 
@@ -59,7 +61,7 @@ fn main() -> ! {
         &mut lcd,
         (0, 0),
         (width, height),
-        0u16,
+        RawU16::from(0u16),
     );
 
     let mut left = 1.0f32;
@@ -93,7 +95,7 @@ fn main() -> ! {
             &mut lcd,
             (ileft, itop),
             (ileft + ball_width - 1, itop + ball_height - 1),
-            0u16,
+            RawU16::from(0u16),
         );
 
         // Update ball position.
@@ -113,7 +115,7 @@ fn main() -> ! {
             &mut lcd,
             (ileft, itop),
             (ileft + ball_width - 1, itop + ball_height - 1),
-            BALL_COLORS[c / 24],
+            RawU16::from(BALL_COLORS[c / 24]),
         );
 
         // Delay to show ball.
